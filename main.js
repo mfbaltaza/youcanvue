32752387e0121129adf40ad9eb1a8b8e9d3cd99e
mfbaltaza <araujogreydiego@gmail.com>
2021-02-08 01:44:03 -0400

    Adding lists and fixing some text

--- main.js
+++ main.js
@@ -5,6 +5,19 @@ const app = new Vue({
 		description: 'Very comfortable ones',
 		image: './assets/vmSocks-green-onWhite.jpg',
 		inventory: 100,
+		onSale: true,
+		details: ["80% cotton", "20% polyester", "Gender-neutral"],
+		variants: [
+			{
+				variantId: 2234,
+				variantColor: "Green"
+			},
+			{
+				variantId: 2235,
+				variantColor: "Blue"
+			}
+		],
+		sizes: ["S", "M", "L", "XL"],
 		github: 'https://github.com/mfbaltaza'
 	}
 });

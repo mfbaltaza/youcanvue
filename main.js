5f5efd1d882ac87d51f9726c44c150cf2da85e96
mfbaltaza <araujogreydiego@gmail.com>
2021-02-07 22:43:14 -0400

    Here we added some and conditional logic

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ const app = new Vue({
 	el: '#app',
 	data: {
 		product: 'Socks',
-		description: 'Very comfortable ones'
+		description: 'Very comfortable ones',
+		image: './assets/vmSocks-green-onWhite.jpg',
+		inventory: 100,
+		github: 'https://github.com/mfbaltaza'
 	}
 });

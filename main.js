f2ccb82c4a11ce69a8d95ae673046f2c8deb46f2
mfbaltaza <araujogreydiego@gmail.com>
2021-02-11 20:27:19 -0400

    Added components and did some work with Computed Properties we also did some Class Binding and Style Binding all of this learning we deleted inStock and improved that system

--- main.js
+++ main.js
@@ -1,28 +1,88 @@
-const app = new Vue({
-	el: '#app',
-	data: {
+Vue.component('product', {
+	props: {
+		premium: {
+			type: Boolean,
+			required: true,
+			default: "Hi"
+		}
+	},
+	template: `
+	<div class="product">
+
+        <div class="product-image">
+          <img v-bind:src="image" alt="Product on sale image" />
+        </div>
+
+        <div class="product-info">
+          <h1> {{ title }} </h1>
+          <p> {{ goingSale }} </p>
+          <p><b> <i> {{ description }} </i> </b></p>
+					<p v-if="inStock">In Stock</p>
+					<p v-else 
+            :style="styleObject">Out of Stock</p>
+			<p>Shipping: {{ shipping }}</p>
+
+					<ul>
+						<li v-for="detail in details">{{ detail }}</li>
+					</ul>
+
+					<p>Colors</p>
+
+					<div v-for="(variant, index) in variants"
+              :key="variant.variantId"
+              class="color-box"
+              :style="{ backgroundColor: variant.variantColor }"
+              @mouseover="updateProduct(index)">
+					</div>
+
+					<p style="font-weight: 800;">Sizes</p>
+					<ul>
+						<li v-for="size in sizes">{{ size }}</li>
+					</ul>
+
+					<button v-on:click="addToCart"
+              :disabled="!inStock"
+              :class="{ disabledButton: !inStock }">Add to cart</button>
+					<button @click="removeFromCart">Remove from the cart</button>
+
+					<div class="cart">Cart({{cart}})</div>
+        </div>
+      </div>
+
+			<div class="extra">
+				<a :href="github">My Github</a>
+			</div>
+`,
+data() {
+	return {
+		brand: 'Vue Mastery',
 		product: 'Socks',
 		description: 'Very comfortable ones',
-		image: './assets/vmSocks-green-onWhite.jpg',
-		inventory: 100,
+		selectedVariant: 0,
 		onSale: true,
 		details: ["80% cotton", "20% polyester", "Gender-neutral"],
 		variants: [
 			{
 				variantId: 2234,
 				variantColor: "Green",
-				variantImage: './assets/vmSocks-green-onWhite.jpg'
+				variantImage: './assets/vmSocks-green-onWhite.jpg',
+				variantQuantity: 10
 			},
 			{
 				variantId: 2235,
 				variantColor: "Blue",
-				variantImage: './assets/vmSocks-blue-onWhite.jpg'
+				variantImage: './assets/vmSocks-blue-onWhite.jpg',
+				variantQuantity: 0
 			}
 		],
 		sizes: ["S", "M", "L", "XL"],
 		cart: 0,
+		styleObject: {
+			textDecoration: 'line-through',
+		},
 		github: 'https://github.com/mfbaltaza',
-	},
+	}
+},
 	methods: {
 		addToCart() {
 			this.cart += 1
@@ -30,8 +90,38 @@ const app = new Vue({
 		removeFromCart() {
 			this.cart -= 1
 		},
-		updateProduct(variantImage) {
-			this.image = variantImage
+		updateProduct(index) {
+			this.selectedVariant = index
+			console.log(index)
 		}
 	},
+	computed: {
+		title() {
+			return this.brand + ' ' + this.product
+		},
+		image() {
+			return this.variants[this.selectedVariant].variantImage
+		},
+		inStock() {
+			return this.variants[this.selectedVariant].variantQuantity
+		},
+		goingSale() {
+			if (this.onSale) {
+				return this.brand + ' ' + this.product + ' ARE ON SALE!!!'
+			}
+		},
+		shipping() {
+			if (this.premium) {
+				return "Free"
+			}
+			return 2.99
+		}
+	}
+})
+
+const app = new Vue({
+	el: '#app',
+	data: {
+		premium: true,
+	}
 });
